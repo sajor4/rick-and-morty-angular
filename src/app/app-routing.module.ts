7899c59040acb8730b50315b693a06da40d08ad6
sajor4 <132441341+sajor4@users.noreply.github.com>
2023-09-25 11:39:24 +0200

    commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home' , component: CharactersListComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'**', redirectTo:'home', pathMatch:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
